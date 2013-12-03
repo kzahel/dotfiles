6a50eff4c071dfeda07637cd46cc54dbe62420a5
Kyle Graehl <kgraehl@gmail.com>
2013-12-03 12:54:22 +0900

    setup

---
+++ chromeos/misc/setup.sh
@@ -0,0 +1,8 @@
+#" setting up a new chromeos device to my liking
+
+sudo apt-get install emacs-goodies-el
+sudo apt-get install emacs23-noX
+sudo apt-get install nano
+sudo apt-get install git
+sudo apt-get install openssh-server
+
